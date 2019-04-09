db693dc54f9fa159add55ec72f62eada1929be62
Dauren Delmukhambetov <d.delmukhambetov@osdkz.com>
2019-04-09 21:38:51 +0600

    Init commit

---
+++ src/main/java/kz/oku/kerek/okukerek/OkuKerekApplication.java
@@ -0,0 +1,13 @@
+package kz.oku.kerek.okukerek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkuKerekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkuKerekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/oku/kerek/okukerek/OkuKerekApplicationTests.java
@@ -0,0 +1,16 @@
+package kz.oku.kerek.okukerek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OkuKerekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
