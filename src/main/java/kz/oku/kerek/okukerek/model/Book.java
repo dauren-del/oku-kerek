fadf01f1bc7170c0d1829593463874bfb5c8cf5f
Dauren Delmukhambetov <d.delmukhambetov@osdkz.com>
2019-04-09 22:45:43 +0600

    Model class added

---
+++ src/main/java/kz/oku/kerek/okukerek/model/Book.java
@@ -0,0 +1,15 @@
+package kz.oku.kerek.okukerek.model;
+
+/**
+ *
+ * @author Dauren Delmukhambetov
+ * @since 0.1.0
+ */
+
+public class Book extends Literature {
+
+    public Book() {
+        super(LiteratureType.BOOK);
+    }
+
+}
---
+++ src/main/java/kz/oku/kerek/okukerek/model/LiteratureType.java
@@ -0,0 +1,19 @@
+package kz.oku.kerek.okukerek.model;
+
+/**
+ *
+ * @author Dauren Delmukhambetov
+ * @since 0.1.0
+ */
+
+public enum LiteratureType {
+
+    BOOK,
+
+    MAGAZINE,
+
+    NEWSPAPER,
+
+    PAPER,
+
+}
