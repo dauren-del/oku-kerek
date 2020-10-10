b506583374be740b6360f6f392685955e41411fc
Dauren Delmukhambetov <dauren.del@gmail.com>
2020-10-10 17:14:27 +0300

    Some fixes

--- src/test/java/kz/oku/kerek/controller/BookRestControllerIntergrationTest.java
+++ src/test/java/kz/oku/kerek/controller/BookRestControllerIntergrationTest.java
@@ -9,11 +9,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties = {"AWS_ACCESS_KEY=AKIAVL7Y3X5SWTO52ZNC", "AWS_SECRET_KEY=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"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookRestControllerIntergrationTest {
 
@@ -23,9 +25,9 @@ public class BookRestControllerIntergrationTest {
     @Test
     public void testFindById_whenBookExists_thenReturnBook() {
         Long bookId = 24L;
-        ResponseEntity<Book> entity = this.template.getForEntity("/book/"+bookId, Book.class);
+        ResponseEntity<Book> entity = this.template.getForEntity("/book/" + bookId, Book.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals(MediaType.APPLICATION_JSON_UTF8, entity.getHeaders().getContentType());
+        assertEquals(MediaType.APPLICATION_JSON, entity.getHeaders().getContentType());
         Book response = entity.getBody();
         assertEquals(bookId, response.getId());
     }
